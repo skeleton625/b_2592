e684fda6d4af1229d5614a887a525533134fddfd
WaniFi <skelpd24@gmail.com>
2018-08-06 22:14:29 +0900

    2592 문제 풀이 완료

--- baekjoon_2592/main.cpp
+++ baekjoon_2592/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int num, avg, c, j, m, mn;
+int n[10], nc[10];
+
 int main(){
+	for (int i = 0; i < 10; i++) {
+		cin >> num;
+		avg += num;
+		for (j = c; j >= 0; j--) {
+			if (n[j] == num) {
+				nc[j]++;
+				break;
+			}
+		}
+		if (j == -1) {
+			nc[c]++;
+			n[c++] = num;
+		}
+	}
+
+	m = nc[0];
+	mn = n[0];
+	for (int i = 1; i < c; i++) 
+		if (nc[i] > m) {
+			mn = n[i];
+			m = nc[i];
+		}
+	cout << avg / 10 << endl;
+	cout << mn;
 	return 0;
 }
